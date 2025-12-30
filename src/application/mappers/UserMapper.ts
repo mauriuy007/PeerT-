822f8cb8d204ad035452fa7f55112b219f1d6914
Mauricio Parodi <mauricio.parodivar@gmail.com>
2025-12-30 15:03:46 -0300

    working on user repo method add, there are still some errors that will be worked out later

--- src/application/mappers/UserMapper.ts
+++ src/application/mappers/UserMapper.ts
@@ -1,11 +1,14 @@
-import {CreateUserDto} from "./"
+import {CreateUserDto} from "../dtos/user/CreateUserDto"
+import { User } from "../../domain/entities/User";
 
 export class UserMapper {
   static mapDtoToEntity(dto: CreateUserDto): User {
     return {
-      id: dto.id,
-      name: dto.name,
-      email: dto.email,
+      id: 1, // Temporary ID, should be set by the database
+      password: { value: dto.password },
+      lastName: { value: dto.lastName },
+      name: { value: dto.name },
+      email: { value: dto.email },
     };
   }
 }
--- src/dataAccess/prisma/UserRepo.ts
+++ src/dataAccess/prisma/UserRepo.ts
@@ -1,1 +1,10 @@
-export class UserRepo 
+// src/dataAccess/prisma/UserRepo.ts
+import { IUserRepo } from "../../domain/repoInterfaces/IUserRepo";
+import { User } from "../../domain/entities/User";
+
+export class UserRepo implements IUserRepo {
+  async create(user: User): Promise<number> {
+    // prisma.user.create({ data: { ... } })
+    return 1;
+  }
+}
